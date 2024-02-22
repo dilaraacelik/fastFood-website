186f8c02f33d1a733407cbfa9207ed176a20e7bf
dilara <dilara.dcelik@gmail.com>
2024-02-22 18:07:58 +0300

    finish header part

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
  
+
+// add eventListeners to multiple elements 
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i=0, len=elements.length; i<len; ++i){
+        elements[i].addEventListener(eventType, callback);}
+}
+
+// select the elements  
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+// function for the navbarTogglers' eventListeners 
+
+const togglerNav = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+addEventOnElements(navbarTogglers, "click", togglerNav);
+
+
+// function for the navbarLinks' eventListeners 
+
+const closeNav = function() {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+addEventOnElements(navbarLinks, "click", closeNav);
+
+//Change header appearance based on scroll position 
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function(){
+    header.classList[window.scrollY > 50 ?  "add" : "remove"]("active");
+});
